9d606188c7687032edadb4425dfbcccced340102
Puneet <yadavpuneet399@gmail.com>
2026-04-24 13:40:50 +0530

    initialzed_git_repo

---
+++ src/main/java/com/bot/mybot/MybotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.mybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybotApplication.class, args);
+	}
+
+}
